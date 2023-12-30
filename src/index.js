ee9e52398d04f202c7f4caaec24a53f9fb136cc3
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2023-12-30 20:07:54 +0200

    release

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 // import StarRating from "./StarRating";
 
 // import CurrencyConverter from "./Challenge/CurrencyConverter";
 // import Comp from "./Challenge/TextExpander";i
-import Geolocation from "./Challenge/Geolocation";
+// import Geolocation from "./Challenge/Geolocation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>{<Geolocation />}</React.StrictMode>);
+root.render(<React.StrictMode>{<App />}</React.StrictMode>);
